2cc53ab586c21ba45beedfd9d1b7fc938126dc21
Gelu Neagu <modai7940@gmail.com>
2025-09-17 20:58:46 +0300

    Add files via upload

---
+++ data/slide2.js
@@ -0,0 +1,36 @@
+(function(){var loadHandler=window['sl_{DFD1B555-CCEF-4A29-8DF3-B18C4E88A467}'];loadHandler&&loadHandler(1, '<div id="spr0_ff2414f"><div id="spr1_ff2414f" class="kern slide"><img id="img6_ff2414f" src="data/img11.jpg" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_ff2414f" class="kern slide"><div id="svg0_ff2414f" style="left:999.588px;top:631.948px;"><svg width="281" height="89" viewBox="0 0 281 89"><path fill="#92d050" fill-opacity="0.2" d="M0,0 h280.412 v88.052 h-280.412 Z"/></svg></div><div id="spr3_ff2414f" style="left:999.588px;top:631.948px;"><div style="width:0px;"><span id="txt0_ff2414f" data-width="210.246094" style="left:61.509px;top:5.706px;">= Rapsodii de primăvară=</span></div><div style="width:0px;"><span id="txt1_ff2414f" data-width="210.246094" style="left:60.6px;top:4.797px;">= Rapsodii de primăvară=</span></div><div style="width:0px;"><span id="txt2_ff2414f" data-width="254.429688" style="left:17.414px;top:28.012px;">muzica și interpret: Tudor Gheorghe</span></div><div style="width:0px;"><span id="txt3_ff2414f" data-width="254.429688" style="left:16.6px;top:27.198px;">muzica și interpret: Tudor Gheorghe</span></div><div style="width:0px;"><span id="txt4_ff2414f" class="nokern" data-width="50.127930" style="left:54.672px;top:47.137px;">acomp.:</span></div><div style="width:0px;"><span id="txt5_ff2414f" class="nokern" data-width="116.716797" style="left:108.339px;top:47.137px;">Orchestra Național</span></div><div style="width:0px;"><span id="txt6_ff2414f" class="nokern" style="left:224.839px;top:47.137px;">ă</span></div><div style="width:0px;"><span id="txt7_ff2414f" class="nokern" data-width="35.512695" style="left:236.005px;top:47.137px;">Radio</span></div><div style="width:0px;"><span id="txt8_ff2414f" class="nokern" data-width="50.127930" style="left:53.933px;top:46.398px;">acomp.:</span></div><div style="width:0px;"><span id="txt9_ff2414f" class="nokern" data-width="116.716797" style="left:107.6px;top:46.398px;">Orchestra Național</span></div><div style="width:0px;"><span id="txt10_ff2414f" class="nokern" style="left:224.1px;top:46.398px;">ă</span></div><div style="width:0px;"><span id="txt11_ff2414f" class="nokern" data-width="35.512695" style="left:235.267px;top:46.398px;">Radio</span></div><div style="width:0px;"><a id="hl0_ff2414f" class="relpos" href="https://www.youtube.com/watch?v=VloxTRz7_zw" title="https://www.youtube.com/watch?v=VloxTRz7_zw" target="_blank" onclick="document.getElementById(\'coreSpr_15875791\').getCore().processTriggerEffect(this);return document.getElementById(\'coreSpr_15875791\').getCore().gotoLink(this);"><span id="txt12_ff2414f" class="relpos" data-width="124.078125" style="left:133.414px;top:64.012px;">sursa:Youtube.co</span></a><a id="hl1_ff2414f" class="relpos" href="https://www.youtube.com/watch?v=VloxTRz7_zw" title="https://www.youtube.com/watch?v=VloxTRz7_zw" target="_blank" onclick="document.getElementById(\'coreSpr_15875791\').getCore().processTriggerEffect(this);return document.getElementById(\'coreSpr_15875791\').getCore().gotoLink(this);"><span id="txt13_ff2414f" class="relpos" style="left:133.407px;top:64.012px;">m</span></a></div><div style="width:0px;"><a id="hl2_ff2414f" class="relpos" href="https://www.youtube.com/watch?v=VloxTRz7_zw" title="https://www.youtube.com/watch?v=VloxTRz7_zw" target="_blank" onclick="document.getElementById(\'coreSpr_15875791\').getCore().processTriggerEffect(this);return document.getElementById(\'coreSpr_15875791\').getCore().gotoLink(this);"><span id="txt14_ff2414f" class="relpos" data-width="124.078125" style="left:132.6px;top:63.198px;">sursa:Youtube.co</span></a><a id="hl3_ff2414f" class="relpos" href="https://www.youtube.com/watch?v=VloxTRz7_zw" title="https://www.youtube.com/watch?v=VloxTRz7_zw" target="_blank" onclick="document.getElementById(\'coreSpr_15875791\').getCore().processTriggerEffect(this);return document.getElementById(\'coreSpr_15875791\').getCore().gotoLink(this);"><span id="txt15_ff2414f" class="relpos" style="left:132.593px;top:63.198px;">m</span></a></div></div><div id="spr4_ff2414f" style="left:310px;top:60px;"><img id="img0_ff2414f" src="data/img6.png" width="257" height="316" alt="Sus, pe câmpul amorțit,\
+A trecut în taină mare,\
+De cu seară, risipind\
+Șiruri de mărgăritare\
+Din panere de argint,\
+Stol bălai\
+De îngerași\
+Cu alai\
+De toporași.\
+Primăvară, cui le dai?\
+Primăvară, cui le lași?"/></div><div id="spr5_ff2414f" style="left:16px;top:454px;"><img id="img1_ff2414f" src="data/img7.png" width="393" height="229" alt="Se-nalță abur moale din gradină,\
+Pe jos, pornesc furnicile la drum.\
+Acoperișuri veștede-n lumina\
+Întind spre cer ogeacuri făr-de fum.\
+Pe lângă garduri s-a uscat pământul\
+Și ies gândacii - Domnului pe zid.\
+Ferestre amorțite se deschid\
+Să intre-n casă soarele și vântul."/></div><div id="spr6_ff2414f" style="left:612px;top:60px;"><img id="img2_ff2414f" src="data/img8.png" width="186" height="345" alt="De prin balcoane\
+Și coridoare\
+Albe tulpane\
+Fâlfâie-n soare.\
+Ies gospodinele,\
+Iuți ca albinele,\
+Părul le flutură,\
+Toate dau zor.\
+Unele matură,\
+Altele scutură\
+Colbul din pătură\
+Și din covor."/></div><div id="spr7_ff2414f" style="left:443px;top:457px;"><img id="img3_ff2414f" src="data/img9.png" width="408" height="228" alt="Un zarzăr mic, în mijlocul grădinii,\
+Și-a răsfirat crenguțele ca spinii\
+De teamă să nu-i cadă la picioare\
+Din creștet, vălul subțirel de floare.\
+Că s-a trezit aşa de dimineață\
+Cu ramuri albe - și se poate spune\
+Că-i pentru prima oară-n viaţă\
+Când i se-ntâmplă asemenea minune."/></div><div id="spr8_ff2414f" style="left:9px;top:10px;"><img id="img4_ff2414f" src="data/img10.png" width="238" height="341" alt=""/></div><div id="spr9_ff2414f" style="left:886.513px;top:10.133px;"><div style="width:0px;"><span id="txt16_ff2414f" data-width="343.234375" style="left:9.6px;top:6.424px;">Rapsodii de primăvară</span></div><div style="width:0px;"><span id="txt17_ff2414f" data-width="221.767502" style="left:150.56px;top:44.283px;">de George Topîrceanu</span></div><div style="width:0px;"><span id="txt18_ff2414f" data-width="105.507088" style="left:264.6px;top:69.883px;">(fragment)</span></div></div><div id="spr10_ff2414f" style="left:1055px;top:104px;"><img id="img5_ff2414f" src="data/img4.png" width="225" height="225" alt=""/></div></div></div>', '{"s":[]}');})();
